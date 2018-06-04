6869abeba8a3080034a6c8ae937369e9b220244b
root <contact@taymiya.com>
2018-06-04 17:34:43 +0000

    send

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Ibnab\Bundle\SaadBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Oro\Bundle\ConfigBundle\DependencyInjection\SettingsBuilder;
+/**
+ * This is the class that validates and merges configuration from your app/config files
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('ibnab_saad');
+        return $treeBuilder;
+    }
+
+    
+}
---
+++ IbnabSaadBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Ibnab\Bundle\SaadBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class IbnabSaadBundle extends Bundle
+{
+
+}
